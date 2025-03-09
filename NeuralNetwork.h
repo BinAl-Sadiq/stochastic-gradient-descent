259fb1a9e408e4b2eaa22a28813447becd0b7904
BinAl-Sadiq <166337968+BinAl-Sadiq@users.noreply.github.com>
2025-03-09 10:51:57 +0300

    Update NeuralNetwork.h

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -13,12 +13,15 @@ public:
 	
 	func_ptr activations;
 	func_ptr activations_derivatives;
+	double(*criterion)(std::vector<double> p, std::vector<double> y);
+	double(*criterion_derivative)(double p, double y);
 
 public:
 	NeuralNetwork() = delete;
-	NeuralNetwork(std::vector<uint32_t> layers_lengths, func_ptr activations, func_ptr activations_derivatives);
+	NeuralNetwork(std::vector<uint32_t> layers_lengths, func_ptr acts, func_ptr acts_d, double(*c)(std::vector<double> p, std::vector<double> y), double(*c_d)(double p, double y));
 
 public:
 	void forward_pass(std::vector<double> inputs);
+	double loss(std::vector<double> desired_outputs);
 	void backward_pass(std::vector<double> desired_outputs, double learning_rate);
 };
